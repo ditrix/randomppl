ef212d51892340bfcfd8df212346a9c4b2fc8486
ditrix <dmvoloshin@gmail.com>
2020-08-19 14:49:45 +0300

    done

--- README.md
+++ README.md
@@ -9,21 +9,7 @@ USE:
 
 RandomuserMe API (https://randomuser.me/)
 
-
 [Create React App](https://github.com/facebook/create-react-app).
-
-[Redux] (https://github.com/reduxjs/redux)
-
-[React-Redux] (https://github.com/reduxjs/react-redux)
-
-[Redux-Thunk] (https://github.com/reduxjs/redux-thunk)
-
-[Redux-Saga] (https://github.com/redux-saga/redux-saga/tree/master/packages/core)
-
-[React-Router] (https://github.com/ReactTraining/react-router)
-
-[Redux-Form] (https://github.com/redux-form/redux-form)
 
 [axios] (https://github.com/axios/axios)
 
-[Redux-Developer-Extensions] (https://github.com/zalmoxisus/redux-devtools-extension)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import axios from 'axios'
+import Person from './Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [],
+    }
+
+  }
+
+  componentDidMount(){
+
+    axios('https://api.randomuser.me/?results=10')
+      .then(response => this.setState({data:response.data.results}))
+      .catch(error => console.error(error))
+  }
+
+  render(){
+    return (
+      <div className="App">
+         <h1>todo randomppl</h1>
+         {(this.state.data === [])?<span>loading...</span>
+           :<ul>{this.state.data.map((item,index) => <li key={index}>{Person(item)}</li> )}</ul>
+         }
+      </div>
+  )}
 }
 
 export default App;
